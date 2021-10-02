5e2b8bdc9fe65d1e4e5908e3b754ef4cf70b9fe6
Ye Thiha <mightycryptowall@gmail.com>
2021-10-02 10:35:04 +0630

    auth -g -a tasks

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,20 +1,27 @@
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
+  @Post('/signup')
+  signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentialsDto);
+  }
 
-    constructor(private authService: AuthService){}
+  @Post('/signin')
+  signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 
-    @Post('/signup')
-    signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
-        return this.authService.signUp(authCredentialsDto);
-    }
-
-    @Post('/signin')
-    signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<string> {
-        return this.authService.signIn(authCredentialsDto);
-    }
+  // @Post('/test')
+  // @UseGuards(AuthGuard())
+  // test(@Req() req) {
+  //   console.log(req);
+  // }
 }
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -2,12 +2,25 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksService } from './tasks.service';
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { TaskStatus } from './task-status.enum';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { Task } from './task.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
@@ -16,9 +29,8 @@ export class TasksController {
     return this.tasksService.getTasks(filterDto);
   }
 
-
   @Get('/:id')
-  getTaskById(@Param('id') id:string): Promise<Task> {
+  getTaskById(@Param('id') id: string): Promise<Task> {
     return this.tasksService.getTaskById(id);
   }
 
@@ -32,15 +44,15 @@ export class TasksController {
   //   @Param('id') id: string,
   //   @Body() updateTaskDto: UpdateTaskDto
   // ): Task{
-  //   return this.tasksService.updaeTask(id, updateTaskDto); 
+  //   return this.tasksService.updaeTask(id, updateTaskDto);
   // }
 
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body() updateTaskStatusDto: UpdateTaskStatusDto
-  ): Promise<Task>{
-    const { status } = updateTaskStatusDto; 
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Promise<Task> {
+    const { status } = updateTaskStatusDto;
     return this.tasksService.updateTaskStatus(id, status);
   }
 
